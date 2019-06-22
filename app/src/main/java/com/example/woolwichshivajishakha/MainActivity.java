295c2914a95781a732432a41b17a7309699405d8
devsevani <devsevani@hotmail.co.uk>
2019-06-22 22:31:53 +0100

    Login button page created

--- app/src/main/java/com/example/woolwichshivajishakha/MainActivity.java
+++ app/src/main/java/com/example/woolwichshivajishakha/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.woolwichshivajishakha;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = (Button)findViewById(R.id.Login);
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+
+            }
+
+        });
     }
 }
