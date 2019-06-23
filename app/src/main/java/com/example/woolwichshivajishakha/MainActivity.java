8f4bbf57783fea7042763971255f23b724de7952
devsevani <devsevani@hotmail.co.uk>
2019-06-23 19:32:10 +0100

    Login page complete

--- app/src/main/java/com/example/woolwichshivajishakha/MainActivity.java
+++ app/src/main/java/com/example/woolwichshivajishakha/MainActivity.java
@@ -2,17 +2,28 @@ package com.example.woolwichshivajishakha;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     EditText edtEmail, edtPassword;
     Button buttonLogin;
     TextView txtSlogan;
+    private FirebaseAuth database;
+    ProgressDialog mDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,48 +33,46 @@ public class MainActivity extends AppCompatActivity {
         edtEmail = (EditText)findViewById(R.id.edtEmail);
         buttonLogin = (Button)findViewById(R.id.Login);
         txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+        mDialog = new ProgressDialog(MainActivity.this);
 
         //Initialise firebase database
-        //FirebaseDatabase database = FirebaseDatabase.getInstance();
-        //final DatabaseReference table_login = database.getReference("Login");
+        database = FirebaseAuth.getInstance();
+        //DatabaseReference table_login = database.getReference("Login");
 
         buttonLogin.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view){
-                final ProgressDialog mDialog = new ProgressDialog(MainActivity.this);
                 mDialog.setMessage("Logging in..");
                 mDialog.show();
-
-                //table_login.addValueEventListener(new ValueEventListener() {
-                    //@Override
-                    //public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        //mDialog.dismiss();
-                        //Check if user does not exist in database
-                        //if(dataSnapshot.child(edtEmail.getText().toString()).exists()) {
-                            //Gets user information
-                            //Login login = dataSnapshot.child(edtEmail.getText().toString()).getValue(Login.class);
-                            //if (login.getPassword().equals(edtPassword.getText().toString())) {
-                                //Toast.makeText(MainActivity.this, "Namaste!", Toast.LENGTH_SHORT).show();
-                            //} else {
-                                //Toast.makeText(MainActivity.this, "Incorrect password. Please check you have entered the correct credentials.", Toast.LENGTH_SHORT).show();
-                            //}
-                        //}
-                        //else{
-                            //Toast.makeText(MainActivity.this, "Login Failed. Please check you have entered the correct details", Toast.LENGTH_SHORT).show();
-                        //}
-
-
-                    //}
-
-                    //@Override
-                    //public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    //}
-                //});
+        logIn();
             }
-
         });
+
+    }
+    //Method to check credentials
+    private void logIn(){
+        String email = edtEmail.getText().toString();
+        String password = edtPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            mDialog.dismiss();
+            Toast.makeText(MainActivity.this, "Email or Password field cannot be empty", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            database.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        mDialog.dismiss();
+                        Toast.makeText(MainActivity.this, "Namaste!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        mDialog.dismiss();
+                        Toast.makeText(MainActivity.this, "Login Failed. Please check you have entered the correct details", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
 
     }
 }
