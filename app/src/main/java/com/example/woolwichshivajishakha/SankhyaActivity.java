b1d990131e0d50f15414f8f617cd13d0d3d36707
devsevani <devsevani@hotmail.co.uk>
2019-06-30 22:32:16 +0100

    Logo and banner improved. (Most likely the final ones), also added Bottom navigation bar on Sankhya page.

--- app/src/main/java/com/example/woolwichshivajishakha/SankhyaActivity.java
+++ app/src/main/java/com/example/woolwichshivajishakha/SankhyaActivity.java
@@ -1,14 +1,44 @@
 package com.example.woolwichshivajishakha;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class SankhyaActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_addSankhya:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_viewSankhya:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_monitorSankhya:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sankhya);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
