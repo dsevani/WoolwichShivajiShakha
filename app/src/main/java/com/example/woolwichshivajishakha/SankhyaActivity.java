3e78066ecfa9b8fe13eca9c2832d1b3637c68e3f
devsevani <devsevani@hotmail.co.uk>
2019-07-01 22:31:23 +0100

    Bottom navigation styles sorted, and also navigate to correct fragments. Fragment layout needs fixing

--- app/src/main/java/com/example/woolwichshivajishakha/SankhyaActivity.java
+++ app/src/main/java/com/example/woolwichshivajishakha/SankhyaActivity.java
@@ -2,43 +2,59 @@ package com.example.woolwichshivajishakha;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SankhyaActivity extends AppCompatActivity {
-    private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_addSankhya:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_viewSankhya:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_monitorSankhya:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private BottomNavigationView nav_bar;
+    private AddSankhya addSankhyaFragment;
+    private ViewSankhya viewSankhyaFragment;
+    private MonitorSankhya monitorSankhyaFragment;
+    private FrameLayout addFrame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sankhya);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        addFrame = (FrameLayout) findViewById(R.id.mainFrame);
+        nav_bar = (BottomNavigationView) findViewById(R.id.nav_view);
+        addSankhyaFragment = new AddSankhya();
+        viewSankhyaFragment = new ViewSankhya();
+        monitorSankhyaFragment = new MonitorSankhya();
+
+        nav_bar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.navigation_addSankhya:
+                        setFragment(addSankhyaFragment);
+                        return true;
+
+                    case R.id.navigation_viewSankhya:
+                        setFragment(viewSankhyaFragment);
+                        return true;
+
+                    case R.id.navigation_monitorSankhya:
+                        setFragment(monitorSankhyaFragment);
+                        return true;
+
+                        default: return false;
+                }
+            }
+        });
+
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.mainFrame,fragment);
     }
 
 }
